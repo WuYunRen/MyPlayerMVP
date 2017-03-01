9b148ac301f75d3f83a0c8c2dbbd04e2eb0ac6fc
shuaishuaishi <shi377833639@163.com>
2017-03-01 21:31:15 +0800

    第er次提交代码

--- app/src/main/java/com/wuyunren/myplayermvp/MainActivity.java
+++ app/src/main/java/com/wuyunren/myplayermvp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wuyunren.myplayermvp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onChildTitleChanged(Activity childActivity, CharSequence title) {
+        super.onChildTitleChanged(childActivity, title);
     }
 }
