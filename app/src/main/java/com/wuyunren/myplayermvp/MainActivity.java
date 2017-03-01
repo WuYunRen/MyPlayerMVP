1ff7a592e57973c83cbb1ab5b7b06892ddce292e
shuaishuaishi <shi377833639@163.com>
2017-03-01 21:11:30 +0800

    第一次提交代码

---
+++ app/src/main/java/com/wuyunren/myplayermvp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wuyunren.myplayermvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
